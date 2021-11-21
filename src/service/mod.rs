0f0019c7e55e154a3df1b4f62fd2e6f9647e6c6d
liul85 <liul85@gmail.com>
2021-11-21 14:23:37 +1100

    Implemented simple service

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,7 +1,86 @@
 mod command_service;
 
-use crate::{CommandResponse, Storage};
+use crate::CommandRequest;
+use crate::KvError;
+use crate::MemTable;
+use crate::{command_request::RequestData, CommandResponse, Storage};
+use std::sync::Arc;
+use tracing::debug;
 
 pub trait CommandService {
     fn execute(self, store: &impl Storage) -> CommandResponse;
+}
+
+pub struct Service<Store = MemTable> {
+    inner: Arc<ServiceInner<Store>>,
+}
+
+impl<Store> Clone for Service<Store> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: Arc::clone(&self.inner),
+        }
+    }
+}
+
+pub struct ServiceInner<Store> {
+    store: Store,
+}
+
+impl<Store: Storage> Service<Store> {
+    pub fn new(store: Store) -> Self {
+        Self {
+            inner: Arc::new(ServiceInner { store }),
+        }
+    }
+
+    pub fn execute(&self, request: CommandRequest) -> CommandResponse {
+        debug!("Got request: {:?}", request);
+        let res = dispatch(request, &self.inner.store);
+        debug!("Executed response {:?}", res);
+        res
+    }
+}
+
+pub fn dispatch(cmd: CommandRequest, store: &impl Storage) -> CommandResponse {
+    match cmd.request_data {
+        Some(RequestData::Hget(param)) => param.execute(store),
+        Some(RequestData::Hgetall(param)) => param.execute(store),
+        Some(RequestData::Hset(param)) => param.execute(store),
+        None => KvError::InvalidCommand("Request has no data".into()).into(),
+        _ => KvError::Internal("Not implemented".into()).into(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::CommandRequest;
+    use crate::Kvpair;
+    use crate::MemTable;
+    use crate::Value;
+    #[test]
+    fn service_should_work() {
+        let service = Service::new(MemTable::default());
+
+        let cloned = service.clone();
+
+        let handle = std::thread::spawn(move || {
+            let res = cloned.execute(CommandRequest::new_hset("t1", "k1", "v1".into()));
+            assert_res_ok(res, &[Value::default()], &[]);
+        });
+
+        handle.join().unwrap();
+
+        let res = service.execute(CommandRequest::new_hget("t1", "k1"));
+        assert_res_ok(res, &["v1".into()], &[])
+    }
+
+    pub fn assert_res_ok(mut res: CommandResponse, values: &[Value], pairs: &[Kvpair]) {
+        res.pairs.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        assert_eq!(res.status, 200);
+        assert_eq!(res.message, "");
+        assert_eq!(res.values, values);
+        assert_eq!(res.pairs, pairs);
+    }
 }
